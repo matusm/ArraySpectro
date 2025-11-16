ea9d091d2ab80f955612f5e6cbba7d7d3a578a8a
Michael Matus <michael@matus.at>
2025-11-16 22:15:53 +0100

    Add project files.

---
+++ ArraySpectro/Helper.cs
@@ -0,0 +1,51 @@
+﻿using At.Matus.OpticalSpectrumLib;
+using Bev.Instruments.Thorlabs.Ctt;
+using System;
+using System.Text;
+
+namespace ArraySpectro
+{
+    internal static class Helper
+    {
+
+        // Updates the provided MeasuredSpectrum by taking numberSamples scans from the tlCcs device.
+        internal static void UpdateSpectrumUI(MeasuredOpticalSpectrum spectrum, int numberSamples, string message, ThorlabsCct spectro)
+        {
+            Console.WriteLine($"Press any key to start measurement of {message} - 's' to skip");
+            if (Console.ReadKey(true).Key != ConsoleKey.S)
+            {
+                ConsoleProgressBar consoleProgressBar = new ConsoleProgressBar();
+                for (int i = 0; i < numberSamples; i++)
+                {
+                    spectrum.UpdateSignal(spectro.GetIntensityData());
+                    consoleProgressBar.Report(i + 1, numberSamples);
+                }
+                spectrum.Name = $"{message}";
+            }
+            else
+            {
+                Console.WriteLine($"Skipping measurement of {message}");
+            }
+        }
+
+        // Creates and returns a MeasuredSpectrum by taking numberSamples scans from the tlCcs device.
+        internal static MeasuredOpticalSpectrum GetSpectrumUI(int numberSamples, string message, ThorlabsCct spectro)
+        {
+            MeasuredOpticalSpectrum spectrum = new MeasuredOpticalSpectrum(spectro.Wavelengths);
+            UpdateSpectrumUI(spectrum, numberSamples, message, spectro);
+            return spectrum;
+        }
+
+        internal static string ToCsvLines(this IOpticalSpectrum spectrum)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < spectrum.NumberOfPoints; i++)
+            {
+                sb.AppendLine(spectrum.DataPoints[i].ToCsvLine());
+            }
+            return sb.ToString();
+        }
+
+    }
+
+}
---
+++ ArraySpectro/Program.cs
@@ -0,0 +1,54 @@
+﻿using At.Matus.OpticalSpectrumLib;
+using Bev.Instruments.Thorlabs.Ctt;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace ArraySpectro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            var spectro = new ThorlabsCct();
+
+            Console.WriteLine($"Instrument Manufacturer:  {spectro.InstrumentManufacturer}");
+            Console.WriteLine($"Instrument Type:          {spectro.InstrumentType}");
+            Console.WriteLine($"Instrument Serial Number: {spectro.InstrumentSerialNumber}");
+            Console.WriteLine($"Firmware Revision:        {spectro.InstrumentFirmwareVersion}");
+            Console.WriteLine($"Driver Revision:          {spectro.InstrumentElectronicsId}");
+            Console.WriteLine($"Min Wavelength:           {spectro.MinimumWavelength:F2} nm");
+            Console.WriteLine($"Max Wavelength:           {spectro.MaximumWavelength:F2} nm");
+            Console.WriteLine();
+
+            Console.WriteLine("Estimating optimal integration time...");
+            double optimalIntegrationTime = spectro.GetOptimalExposureTime();
+            Console.WriteLine($"Optimal Integration Time: {optimalIntegrationTime} s");
+            Console.WriteLine();
+            spectro.SetIntegrationTime(optimalIntegrationTime);
+
+            int nSamples = 11;
+
+            MeasuredOpticalSpectrum reference = new MeasuredOpticalSpectrum(spectro.Wavelengths);
+            MeasuredOpticalSpectrum dark = new MeasuredOpticalSpectrum(spectro.Wavelengths);
+
+            spectro.OpenShutter();
+            Helper.UpdateSpectrumUI(reference, nSamples, "Reference Spectrum", spectro);
+            spectro.CloseShutter();
+            Helper.UpdateSpectrumUI(dark, nSamples, "Dark Spectrum", spectro);
+            spectro.OpenShutter();
+
+            OpticalSpectrum corrected = SpecMath.Subtract(reference, dark);
+
+            string csvString = corrected.ToCsvLines();
+            string fileName = $"spectrum_{DateTime.Now:yyyyMMdd_HHmmss}.csv";
+            string outPath = Path.Combine(Environment.CurrentDirectory, fileName);
+            File.WriteAllText(outPath, csvString);
+
+            Console.WriteLine("done.");
+
+
+        }
+    }
+}
